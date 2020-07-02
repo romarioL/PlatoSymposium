b6bec8f15cf9e3875853f4c7899bae2abff02d40
Romario Sousa <estreladamanha585@gmail.com>
2020-07-02 15:42:20 -0300

    Começando página home

---
+++ page-home.php
@@ -0,0 +1,36 @@
+<?php  get_header();?>
+<div class="row text-banners container mr-auto ml-auto">
+	<div class="col-lg-4 col-md-12 text-banner">
+		<h6 class="text-banner-title"> Por que os gays devem saber sua história?</h6>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+	</div>
+	<div class="col-lg-4 col-md-12 text-banner">
+		<h6 class="text-banner-title">Por que transativistas  são  tão ditatoriais?</h6>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+	</div>
+	<div class="col-lg-4 col-md-12 text-banner">
+		<h6 class="text-banner-title"> Fatos da história gay que não te contam:</h6>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+	</div>
+</div>
+
+<div class="row articles container mr-auto ml-auto">
+	<?php if(have_posts()): while(have_posts()): the_post() ?>
+	      <?php  get_template_part('template-parts/content', 'home'); ?>
+<?php  endwhile;
+        else: 
+
+        	?>
+
+        	<p>Nenhuma postagem disponível</p>
+
+
+
+        <?php
+
+        endif;
+
+        ;?>
+</div>
+
+<?php get_footer(); ?>
---
+++ template-parts/content-home.php
@@ -0,0 +1,4 @@
+<article class="col-lg-12 article">
+		<a href="<?php the_permalink();?>" class="link-article"><h6 class="article-title"><?php the_title(); ?></h6></a>
+		<p><?php  the_content();?></p>
+</article>
