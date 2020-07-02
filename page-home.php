d68a0526604dbe34047296d1eab6d5e94fd45af7
Romario Sousa <estreladamanha585@gmail.com>
2020-07-02 16:10:40 -0300

    Colocando sidebars na page-home

--- page-home.php
+++ page-home.php
@@ -1,17 +1,30 @@
 <?php  get_header();?>
 <div class="row text-banners container mr-auto ml-auto">
-	<div class="col-lg-4 col-md-12 text-banner">
-		<h6 class="text-banner-title"> Por que os gays devem saber sua história?</h6>
-		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-	</div>
-	<div class="col-lg-4 col-md-12 text-banner">
-		<h6 class="text-banner-title">Por que transativistas  são  tão ditatoriais?</h6>
-		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-	</div>
-	<div class="col-lg-4 col-md-12 text-banner">
-		<h6 class="text-banner-title"> Fatos da história gay que não te contam:</h6>
-		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-	</div>
+	<?php if(is_active_sidebar('home-text1')):
+
+			dynamic_sidebar('home-text1');
+
+	     endif;
+
+	     ?>
+
+	  <?php if(is_active_sidebar('home-text2')):
+
+			dynamic_sidebar('home-text2');
+
+	     endif;
+
+	     ?>
+
+
+	   <?php if(is_active_sidebar('home-text3')):
+
+			dynamic_sidebar('home-text3');
+
+	     endif;
+
+	     ?>
+
 </div>
 
 <div class="row articles container mr-auto ml-auto">
