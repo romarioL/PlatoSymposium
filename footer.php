e834ae1ea0493d3b2473295aa02a176c3324a92a
Romario Sousa <estreladamanha585@gmail.com>
2020-07-03 14:42:14 -0300

    Ajeitando Footer

--- footer.php
+++ footer.php
@@ -3,15 +3,13 @@
 
  <div class="footer">
  	<div class="row">
- 		<?php dynamic_sidebar('Footer'); ?>
+ 		<?php if(is_active_sidebar('Footer')): 
+ 			       dynamic_sidebar('Footer');
+ 			   endif; ?>
  	</div>
  	<div class="copyright">
  		<h6><?php  echo get_theme_mod('sec_copyright');?></h6>
  	</div>
  </div>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="<?php bloginfo('template_url')?>/js/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="<?php bloginfo('template_url') ?>/js/bootstrap.min.js"></script>
   </body>
 </html>
--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ function load_scripts(){
 	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.3.1', true);
 	wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.3.1', 'all');
 	wp_enqueue_style('template', get_template_directory_uri().  '/css/template.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('popper-js', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array('jquery'));
 }
 
 add_action('wp_enqueue_scripts', 'load_scripts');
