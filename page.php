cb7815db136d75e4f93cb99157141e3be38781d4
Romario Sousa <estreladamanha585@gmail.com>
2020-07-03 09:37:14 -0300

    Refatorando page.php

--- page.php
+++ page.php
@@ -2,11 +2,7 @@
 
 <div class="row articles container mr-auto ml-auto">
 	<?php if(have_posts()): while(have_posts()): the_post() ?>
-	<div class="col-lg-12 article">
-		<?php  the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded mb-5')); ?>
-		<h6 class="article-title"><?php the_title(); ?></h6>
-		<p><?php  the_content();?></p>
-	</div>
+		<?php get_template_part('template-parts/content', 'page'); ?>
 <?php  endwhile; endif;?>
 </div>
 <?php get_footer(); ?>
