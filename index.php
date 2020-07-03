aca7954de6968a8f46b45596f84333d22f9f8aef
Romario Sousa <estreladamanha585@gmail.com>
2020-07-03 16:15:54 -0300

    Tamanho popular post slider

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php  get_header();?>
+<div class="popular-post-slider m-5 p-5">
 <?php
 	$design = get_theme_mod('set_popular_posts_option');
 	$limit = get_theme_mod('set_popular_posts_limit');
 	$viewBy =  get_theme_mod('set_popular_posts_counting_option');
 	echo do_shortcode('[wtpsw_popular_post design="design-'.$design.'" limit="'.$limit.'" view_by="'.$viewBy.'"]'); 
 	?>
+</div>
+
 <div class="container">
 
 	<div class="row">
@@ -16,7 +19,7 @@
 			</div>
 			<?php next_posts_link('Anteriores'); ?><?php previous_posts_link('Próximos')?>
 		</div>
-		<div class="col-md-4 mt-5 col-sm-12">
+		<div class=" col-md-4 mt-5 col-sm-12">
 		<div class="row">
 		<?php if(is_active_sidebar('posts-sidebar')):
 			      dynamic_sidebar('posts-sidebar');
