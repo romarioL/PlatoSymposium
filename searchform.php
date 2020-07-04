1678874009876e53aab8f29b319e1f64eaf64fd3
Romario Sousa <estreladamanha585@gmail.com>
2020-07-04 10:30:13 -0300

    Adicionar serach form e página de resultados de busca

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyseventeen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	<button type="submit" class="btn btn-info"><?php echo _x( 'Search', 'submit button', 'twentyseventeen' ); ?></button>
+</form>
