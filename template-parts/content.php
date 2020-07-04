76200d3d888065ff56851c6650b2949f92ad8790
Romario Sousa <estreladamanha585@gmail.com>
2020-07-04 15:30:52 -0300

    Autor, categorias, e datas da tela principal

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,7 @@
 <article class="col-lg-12 article">
 		<a href="<?php the_permalink();?>" class="link-article"><h6 class="article-title"><?php the_title(); ?></h6></a>
+		<p>Published by   <?php the_author_posts_link(); ?> in <?php echo get_the_date(); ?>  <?php the_category();?>  <?php the_tags();?></p>
+
+		<hr>
 		<p><?php  the_content();?></p>
 </article>
