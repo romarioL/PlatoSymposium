d06aeb9f2f0297d201b0730ab708792d1bbe31e7
Romario Sousa <estreladamanha585@gmail.com>
2020-07-05 15:58:41 -0300

    thumbnail content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,5 @@
 <article class="col-lg-12 article">
+	    <?php  the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded mb-5')); ?>
 		<a href="<?php the_permalink();?>" class="link-article"><h6 class="article-title"><?php the_title(); ?></h6></a>
 		<p>Published by   <?php the_author_posts_link(); ?> in <?php echo get_the_date(); ?>  <?php the_category();?>  <?php the_tags();?></p>
 		<hr>
