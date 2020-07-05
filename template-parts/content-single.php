e89af980f2a707d6f6f4f002bc88743b0fcd3459
Romario Sousa <estreladamanha585@gmail.com>
2020-07-05 16:26:47 -0300

    Modificar tamanho post thumbnail

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,5 +1,5 @@
 <article class="col-lg-12 article">
-		<?php  the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded mb-5')); ?>
+		<?php  the_post_thumbnail('medium', array('class' => 'img-fluid rounded mb-5')); ?>
 		<h6 class="article-title"><?php the_title(); ?></h6>
 		<p>Published by   <?php the_author_posts_link(); ?> in <?php echo get_the_date(); ?></p>
 		<hr>
