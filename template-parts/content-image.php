8008cf0a85a37891fa7a26d83561b1ef99bdec08
Romario Sousa <estreladamanha585@gmail.com>
2020-07-05 17:30:54 -0300

    Alterando thumbnail  do content-image.php

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -1,6 +1,6 @@
 <article class="col-lg-12 article">
 		<a href="<?php the_permalink();?>" class="link-article"><h6 class="article-title"><?php the_title(); ?></h6></a>
 		<hr>
-		<p><?php  the_post_thumbnail(array(300,300));?></p>
+        <?php  the_post_thumbnail('medium', array('class' => 'img-fluid rounded mb-5')); ?>
 		<?php the_content(); ?>
 </article>
