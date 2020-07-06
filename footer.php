6c315bc2b4554cf59966efc98fc1beaaf4bcda7e
Romario Sousa <estreladamanha585@gmail.com>
2020-07-06 10:02:47 -0300

    Mudando a cor do footer

--- footer.php
+++ footer.php
@@ -2,16 +2,15 @@
  <?php wp_footer(); ?>
 
  <div class="footer">
- 	<div class="row">
- 		<?php if(is_active_sidebar('Footer')): 
- 			       dynamic_sidebar('Footer');
- 			   endif; ?>
- 	</div>
- 	<div class="row">
-	 	<div class="copyright col-sm-12">
-	 		<h6><?php  echo get_theme_mod('sec_copyright');?></h6>
+ 	<div class="container">
+	 	<div class="row">
+	 		<?php if(is_active_sidebar('Footer')): 
+	 			       dynamic_sidebar('Footer');
+	 			   endif; ?>
 	 	</div>
-   </div>
- </div>
+	</div>
+	<div class="copyright">
+		<h6><?php  echo get_theme_mod('sec_copyright');?></h6>
+	</div> 
   </body>
 </html>
--- template-parts/content-home.php
+++
@@ -1,4 +0,0 @@
-<article class="col-lg-12 article">
-		<a href="<?php the_permalink();?>" class="link-article"><h6 class="article-title"><?php the_title(); ?></h6></a>
-		<p><?php  the_content();?></p>
-</article>
