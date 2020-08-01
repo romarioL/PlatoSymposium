3fc914fd0085cbc6e0aea88974e79ab0d757cbba
Romis Sousa <estreladamanha585@gmail.com>
2020-08-01 13:05:53 -0300

    Fix error on issue #27

--- header.php
+++ header.php
@@ -9,6 +9,14 @@
   </head>
   <?php wp_head(); ?>
   <body <?php body_class(); ?>>
+  <?php
+  if ( function_exists( 'wp_body_open' ) ) {
+    wp_body_open();
+} else {
+    do_action( 'wp_body_open' );
+}
+
+?>
 
 
    <nav class="navbar navbar-expand-md navbar-light bg-faded">
